b20d0113208da99e135658290ea5a12edc605217
uni2237 <a01022371341@gmail.com>
2021-07-25 17:00:53 +0900

    스프링 DB 접근 기술- h2 데이터베이스 / 순수 JDBC

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,12 +1,20 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration // Spring이 이걸 읽고 아래 @Bean들을 spirng bean에 등록한다.
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -15,6 +23,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
